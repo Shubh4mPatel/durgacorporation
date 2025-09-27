29c2f9ef475858f45a1a190a9ea2ef4af48b6d59
harshbhatt <harshbhatt@hyperlinkinfosystem.net.in>
2025-09-28 02:22:58 +0530

    new things added

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,9 @@
 import React from "react";
 import HeroSection from "../components/Hero-section/HeroSection";
 import About from "../components/About/About";
-<<<<<<< HEAD
 import CardSection from "../components/Spices-Card-section/SpicesCardSection";
 import OurProducts from "../components/OurProducts/OurProducts";
-=======
 import Why from "../components/why/Why";
->>>>>>> fa6cf0d45e2d11a752b4cfb519d6821b6ee24ccf
 
 export const page = () => {
   return (
@@ -14,11 +11,8 @@ export const page = () => {
       <HeroSection />
       <CardSection/>
       <About />
-<<<<<<< HEAD
       <OurProducts/>
-=======
       <Why />
->>>>>>> fa6cf0d45e2d11a752b4cfb519d6821b6ee24ccf
     </>
   );
 };
