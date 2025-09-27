41f4c104b8cee698f08fa8d1e814e323a427a7b9
Shubham <shubh4m@dev@gmail.com>
2025-09-27 20:21:38 +0530

    Add HeroSection component and update Header styles for improved layout

---
+++ src/app/components/Hero-section/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './HeroSection.css';
+
+const DurgaCorporation = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <h1 className="main-title">Welcome To Durga Corporation</h1>
+          <p className="description">
+            Durga Corporation is a manufacturer and<br />
+            exporter of spices and herbs from Patan (NG),<br />
+            India. Tapping into Patan, just 30km from<br />
+            Unjha, which is renowned as Asia's biggest<br />
+            spice market
+          </p>
+          <button className="know-more-btn">Know More</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DurgaCorporation;
---
+++ src/app/home/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import HeroSection from '../components/Hero-section/HeroSection'
+
+export const page = () => {
+  return (
+    <HeroSection />
+  )
+}
+export default page
