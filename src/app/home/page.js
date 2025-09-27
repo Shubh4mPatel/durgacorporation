7f1929f2cf2ce0b2b28a8c6b1f742df0c085fc81
Shubham <shubh4m@dev@gmail.com>
2025-09-27 22:49:03 +0530

    Add Why component with styles and integrate into home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/Hero-section/HeroSection";
 import About from "../components/About/About";
+import Why from "../components/why/Why";
 
 export const page = () => {
   return (
     <>
       <HeroSection />
       <About />
+      <Why />
     </>
   );
 };
