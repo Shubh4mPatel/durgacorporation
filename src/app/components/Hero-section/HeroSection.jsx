423722ef6cb59ad8462606ef6e1e956c1092f8b4
Patel Shubham <87634631+N3xus97@users.noreply.github.com>
2025-09-29 16:31:20 +0530

    Enhance layout and styling for HeroSection and OurProducts components; center content and adjust spacing for improved visual hierarchy

--- src/app/components/Hero-section/HeroSection.jsx
+++ src/app/components/Hero-section/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./HeroSection.css";
 
-const DurgaCorporation = ({isButtonVisible = false}) => {
+const DurgaCorporation = ({ isButtonVisible = false }) => {
   return (
     <>
       <div className="container">
@@ -9,13 +9,13 @@ const DurgaCorporation = ({isButtonVisible = false}) => {
           <h1 className="main-title">Welcome To Durga Corporation</h1>
           <p className="description">
             Durga Corporation is a manufacturer and
-            <br />
+
             exporter of spices and herbs from Patan (NG),
-            <br />
+
             India. Tapping into Patan, just 30km from
-            <br />
+
             Unjha, which is renowned as Asia's biggest
-            <br />
+
             spice market
           </p>
           <button
